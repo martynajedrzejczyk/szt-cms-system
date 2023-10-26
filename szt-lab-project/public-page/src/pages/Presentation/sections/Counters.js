c1fbdb203189931fb8125ebfe63009fd379818c6
martyna <jmartyna1810@gmail.com>
2023-10-26 14:13:26 +0200

    init

---
+++ szt-lab-project/public-page/src/pages/Presentation/sections/Counters.js
@@ -0,0 +1,64 @@
+/*
+=========================================================
+* Material Kit 2 React - v2.1.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/material-kit-react
+* Copyright 2023 Creative Tim (https://www.creative-tim.com)
+
+Coded by www.creative-tim.com
+
+ =========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*/
+
+// @mui material components
+import Container from "@mui/material/Container";
+import Grid from "@mui/material/Grid";
+import Divider from "@mui/material/Divider";
+
+// Material Kit 2 React components
+import MKBox from "components/MKBox";
+
+// Material Kit 2 React examples
+import DefaultCounterCard from "examples/Cards/CounterCards/DefaultCounterCard";
+
+function Counters() {
+  return (
+    <MKBox component="section" py={3}>
+      <Container>
+        <Grid container item xs={12} lg={9} sx={{ mx: "auto" }}>
+          <Grid item xs={12} md={4}>
+            <DefaultCounterCard
+              count={70}
+              suffix="+"
+              title="Wyprowadzanych piesków dziennie"
+              description="Wejdź w szczegóły, aby sprawdzić dostępność usług w Twoim mieście"
+            />
+          </Grid>
+          <Grid item xs={12} md={4} display="flex">
+            <Divider orientation="vertical" sx={{ display: { xs: "none", md: "block" }, mx: 0 }} />
+            <DefaultCounterCard
+              count={15}
+              suffix="+"
+              title="Pracowników"
+              description="Wielbicieli psów ze sporym doświadczeniem"
+            />
+            <Divider orientation="vertical" sx={{ display: { xs: "none", md: "block" }, ml: 0 }} />
+          </Grid>
+          <Grid item xs={12} md={4}>
+            <DefaultCounterCard
+              count={100}
+              suffix="+"
+              title="Opinii"
+              description="Potwierdzających wysoką jakość naszych usług"
+            />
+          </Grid>
+        </Grid>
+      </Container>
+    </MKBox>
+  );
+}
+
+export default Counters;
