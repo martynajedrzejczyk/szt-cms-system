c6a5f4ac9386fa371bb48152b377951f4c7eac2b
Krzysztof Dereszyński <krzysztof.dereszynski@gmail.com>
2024-01-05 00:59:42 +0100

    Login finished

--- api/app.py
+++ api/app.py
@@ -1,15 +1,40 @@
-from flask import Flask
+from functools import wraps
 
-from mongoApi import mongo_instance
+import pymongo
+from pymongo.server_api import ServerApi
+from flask import Flask, render_template, session, redirect
 
 app = Flask(__name__)
+app.secret_key = b'\xcc^\x91\xea\x17-\xd0W\x03\xa7\xf8J0\xac8\xc5'
+uri = "mongodb+srv://123:123@sztcluster.edm9ind.mongodb.net/?retryWrites=true&w=majority"
+# Database
+client = pymongo.MongoClient(uri,  server_api=ServerApi('1'))
+db = client["SZT_DB"]
+client.admin.command('ping')
+print("Pinged your deployment. You successfully connected to MongoDB!")
+
+
+# Decorators
+def login_required(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return f(*args, **kwargs)
+        else:
+            return redirect('/')
+
+    return wrap
+
+
+# Routes
+
+
 @app.route('/')
-def base():
-    result = mongo_instance.read("City")
-    for item in result:
-        print(item)
-    return "haha"
+def home():
+    return render_template('home.html')
 
 
-if __name__ == '__main__':
-    app.run(debug=True, port=5001, host='127.0.0.1')
+@app.route('/dashboard/')
+@login_required
+def dashboard():
+    return render_template('dashboard.html')
