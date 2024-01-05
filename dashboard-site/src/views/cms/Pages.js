16a3d5c4c54ef61472bbdd91a5418f19abdc8fa3
martyna <jmartyna1810@gmail.com>
2024-01-05 13:01:51 +0100

    change login page

---
+++ dashboard-site/src/views/cms/Pages.js
@@ -0,0 +1,81 @@
+import React from 'react'
+import { CCol, CRow, CButton, CTable, CFormSwitch } from '@coreui/react'
+import CIcon from '@coreui/icons-react'
+import { cilPencil, cilSettings } from '@coreui/icons'
+import DataTable from 'react-data-table-component';
+
+const columns = [
+    {
+        name: 'ID',
+        selector: row => row.id,
+        sortable: true,
+    },
+    {
+        name: 'Nazwa',
+        selector: row => row.name,
+        sortable: true,
+    },
+    {
+        name: 'Widoczność',
+        selector: row => row.visibility,
+        center: true
+    },
+    {
+        name: 'Edytuj',
+        selector: row => row.edit,
+        button: true
+    }
+];
+
+const data = [
+    {
+        id: 1,
+        name: 'stronka1',
+        visibility: (
+            <CFormSwitch checked={true} />
+        ),
+        edit: (
+            <div
+                onClick={() => {
+                    console.log('edit')
+                }}
+            >
+                <CIcon icon={cilPencil} size="xl" />
+            </div>
+        ),
+    },
+    {
+        id: 2,
+        name: 'stronka2',
+        visibility: (
+            <CFormSwitch checked={true} />
+        ),
+        edit: (
+            <div
+                onClick={() => {
+                    console.log('edit')
+                }}
+            >
+                <CIcon icon={cilPencil} size="xl" />
+            </div>
+        ),
+    },
+]
+
+const Pages = () => {
+    return (
+        <CCol>
+            <CRow>
+                <CCol xs={10}>
+                    <h1>Strony</h1>
+                </CCol>
+                <CCol xs={2}>
+                    <CButton color="primary">Dodaj stronę</CButton>
+                </CCol>
+            </CRow>
+            <DataTable columns={columns} data={data} />
+        </CCol>
+    )
+}
+
+export default Pages
