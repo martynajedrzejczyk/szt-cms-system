6f48e83f58bf66d4db5b09275eb8d6e9267273ef
Krzysztof Dereszyński <krzysztof.dereszynski@gmail.com>
2024-01-07 02:18:34 +0100

    contacts update

--- api/contact/models.py
+++ api/contact/models.py
@@ -40,13 +40,13 @@ class Contact:
                     or 'postal_code' not in data or 'mail' not in data):
                 return jsonify({'status': 'error', 'message': 'Missing required fields'})
             result = db['Contact'].insert_one({'company_name': data['company_name'],
-                                            'phone_number': data['phone_number'],
-                                            'street': data['street'],
-                                            'city': data['city'],
-                                            'postal_code': data['postal_code'],
-                                            'mail': data['mail'],
-                                            'modified_at': datetime.datetime.today(),
-                                            'modified_by': session['user']['_id']})
+                                               'phone_number': data['phone_number'],
+                                               'street': data['street'],
+                                               'city': data['city'],
+                                               'postal_code': data['postal_code'],
+                                               'mail': data['mail'],
+                                               'modified_at': datetime.datetime.today(),
+                                               'modified_by': session['user']['_id']})
 
             if result:
                 return jsonify(data['company_name'], ' successfully inserted.'), 200
@@ -55,21 +55,26 @@ class Contact:
         except Exception as e:
             return jsonify({'status': 'error', 'message': str(e)}), 400
 
-    # def update(self):
-    #     try:
-    #         update_data = request.get_json()
-    #
-    #         if 'name' not in update_data or 'visible' not in update_data:
-    #             return jsonify({'status': 'error', 'message': 'Missing required fields'})
-    #         existing_city = db['City'].find_one({'name': update_data['name']})
-    #         if existing_city:
-    #             db['City'].update_one({'name': update_data['name']}, {'$set': {'name': update_data['name'],
-    #                                                                            'visible': update_data['visible']}})
-    #             return jsonify(update_data['name'], ' successfully updated.'), 200
-    #         else:
-    #             return jsonify({'status': 'error', 'message': f'City {update_data["name"]} not found'}), 400
-    #     except Exception as e:
-    #         return jsonify({'status': 'error', 'message': str(e)}), 400
+    def update(self):
+        try:
+            update_data = request.get_json()
+
+            existing_contact = db['Contact'].find_one({'company_name': update_data['company_name']})
+            if existing_contact:
+                db['City'].update_one({'name': update_data['name']},
+                                      {'$set': {'company_name': update_data['company_name'],
+                                                'phone_number': update_data['phone_number'],
+                                                'street': update_data['street'],
+                                                'city': update_data['city'],
+                                                'postal_code': update_data['postal_code'],
+                                                'mail': update_data['mail'],
+                                                'modified_at': datetime.datetime.today(),
+                                                'modified_by': session['user']['_id']}})
+                return jsonify('Successfully updated.'), 200
+            else:
+                return jsonify({'status': 'error', 'message': f'City {update_data["name"]} not found'}), 400
+        except Exception as e:
+            return jsonify({'status': 'error', 'message': str(e)}), 400
 
     def delete(self):
         try:
