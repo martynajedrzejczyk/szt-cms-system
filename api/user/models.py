46babccf12e5c08f890d6653b937693f27811f24
Krzysztof Dereszyński <krzysztof.dereszynski@gmail.com>
2024-01-13 22:44:17 +0100

    password encryption method change

--- api/user/models.py
+++ api/user/models.py
@@ -1,78 +1,80 @@
 import datetime
 from bson import json_util
 from flask import jsonify, request, session, redirect
-from passlib.hash import pbkdf2_sha256
 from app import db
+import bcrypt
 import uuid
+
 
 class User:
 
-  def start_session(self, user):
-    del user['password']
-    session['logged_in'] = True
-    session['user'] = user
-    return jsonify(user), 200
+    def start_session(self, user):
+        del user['password']
+        session['logged_in'] = True
+        session['user'] = user
+        return jsonify(user), 200
 
-  def signup(self):
-    print(request.form)
+    def signup(self):
+        print(request.form)
 
-    # Create the user object
-    user = {
-      "_id": uuid.uuid4().hex,
-      "name": request.form.get('name'),
-      "surname": request.form.get('surname'),
-      "email": request.form.get('email'),
-      "password": request.form.get('password'),
-      "created_at": datetime.datetime.today(),
-      "last_login": datetime.datetime.today(),
-      "last_ip": str(request.environ.get('HTTP_X_REAL_IP', request.remote_addr))
-    }
-    # Encrypt the password
-    user['password'] = pbkdf2_sha256.encrypt(user['password'])
-    # Check for existing email address
-    if db["User"].find_one({ "email": user['email'] }):
-      return jsonify({ "error": "Email address already in use" }), 400
+        # Create the user object
+        user = {
+            "_id": uuid.uuid4().hex,
+            "name": request.form.get('name'),
+            "surname": request.form.get('surname'),
+            "email": request.form.get('email'),
+            "password": request.form.get('password'),
+            "created_at": datetime.datetime.today(),
+            "last_login": datetime.datetime.today(),
+            "last_ip": str(request.environ.get('HTTP_X_REAL_IP', request.remote_addr))
+        }
+        # Encrypt the password
+        user['password'] = bcrypt.hashpw(user['password'].encode(), bcrypt.gensalt())
+        # Check for existing email address
+        if db["User"].find_one({"email": user['email']}):
+            return jsonify({"error": "Email address already in use"}), 400
 
-    if db["User"].insert_one(user):
-      return self.start_session(user)
+        if db["User"].insert_one(user):
+            return self.start_session(user)
 
-    return jsonify({ "error": "Signup failed" }), 400
-  
-  def signout(self):
-    session.clear()
-    return redirect('/')
-  
-  def login(self):
+        return jsonify({"error": "Signup failed"}), 400
 
-    user = db["User"].find_one({
-      "email": request.form.get('email')
-    })
-    if user and pbkdf2_sha256.verify(request.form.get('password'), user['password']):
-      db["User"].update_one({'email':request.form.get('email')},{'$set':{'last_login':datetime.datetime.today()}})
-      return self.start_session(user)
-    
-    return jsonify({ "error": "Invalid login credentials" }), 401
+    def signout(self):
+        session.clear()
+        return redirect('/')
 
-  def read_all(self):
-    try:
-      document = list(db['User'].find())
-      for user in document:
-        user['_id'] = str(user['_id'])
-      if document:
-        return json_util.dumps(document), 200
-      else:
-        return jsonify({'status': 'error', 'message': 'Users not found'}), 400
-    except Exception as e:
-      return jsonify({'status': 'error', 'message': str(e)}), 400
+    def login(self):
 
-  def read(self):
-    try:
-      data = request.get_json()
-      document = db['User'].find_one({'name': data['name']})
-      if document:
-        document['_id'] = str(document['_id'])
-        return json_util.dumps(document), 200
-      else:
-        return jsonify({'status': 'error', 'message': 'City not found'}), 400
-    except Exception as e:
-      return jsonify({'status': 'error', 'message': str(e)}), 400
+        user = db["User"].find_one({
+            "email": request.form.get('email')
+        })
+        if user and bcrypt.checkpw(request.form.get('password').encode(), user['password']):
+            db["User"].update_one({'email': request.form.get('email')},
+                                  {'$set': {'last_login': datetime.datetime.today()}})
+            return self.start_session(user)
+
+        return jsonify({"error": "Invalid login credentials"}), 401
+
+    def read_all(self):
+        try:
+            document = list(db['User'].find())
+            for user in document:
+                user['_id'] = str(user['_id'])
+            if document:
+                return json_util.dumps(document), 200
+            else:
+                return jsonify({'status': 'error', 'message': 'Users not found'}), 400
+        except Exception as e:
+            return jsonify({'status': 'error', 'message': str(e)}), 400
+
+    def read(self):
+        try:
+            data = request.get_json()
+            document = db['User'].find_one({'name': data['name']})
+            if document:
+                document['_id'] = str(document['_id'])
+                return json_util.dumps(document), 200
+            else:
+                return jsonify({'status': 'error', 'message': 'City not found'}), 400
+        except Exception as e:
+            return jsonify({'status': 'error', 'message': str(e)}), 400
