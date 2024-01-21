84e885a1c5094bad2bdc3467691cbfdae97b95c2
Krzysztof Dereszyński <krzysztof.dereszynski@gmail.com>
2024-01-21 02:00:40 +0100

    images added

--- api/image/routes.py
+++ api/image/routes.py
@@ -2,11 +2,6 @@ from app import app
 from image.models import Image
 
 
-@app.route('/images', methods=['GET'])
-def read_all_images():
-    return Image().read_all()
-
-
 @app.route('/image', methods=['GET'])
 def read_image():
     return Image().read()
@@ -17,9 +12,6 @@ def write_image():
     return Image().write()
 
 
-@app.route('/image', methods=['PUT'])
-def update_image():
-    return Image().update()
-@ app.route('/image', methods=['DELETE'])
+@app.route('/image', methods=['DELETE'])
 def delete_image():
     return Image().delete()
