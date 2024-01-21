64f1d25aaa2ae841ed59040650242e4207fb483e
Krzysztof Dereszyński <krzysztof.dereszynski@gmail.com>
2024-01-21 02:04:07 +0100

    image models refactor

--- api/image/models.py
+++ api/image/models.py
@@ -6,15 +6,20 @@ class Image:
     PUBLIC_FOLDER = 'public'
 
     @staticmethod
+    def _get_image_path(name):
+        return os.path.join(Image.PUBLIC_FOLDER, name)
+
+    @staticmethod
     def read():
         try:
             data = request.get_json()
+            image_path = Image._get_image_path(data['name'])
 
-            image_path = os.path.join(Image.PUBLIC_FOLDER, data['name'])
             if os.path.exists(image_path):
                 return send_file(image_path)
             else:
                 abort(404, 'Image not found')
+
         except Exception as e:
             return {"error": str(e)}
 
@@ -22,22 +27,23 @@ class Image:
     def write():
         try:
             image = request.files['image']
-            image_name = image.name
-            image.save(os.path.join(Image.PUBLIC_FOLDER, image_name))
-            return {"response": "file saved successfully in your current directory"}
+            image.save(os.path.join(Image.PUBLIC_FOLDER, image.name))
+            return {"response": f"File saved successfully in the {Image.PUBLIC_FOLDER} folder"}, 200
+
         except Exception as e:
-            return {"error": str(e)}
+            return {"error": str(e)}, 400
 
     @staticmethod
     def delete():
         try:
             data = request.get_json()
+            image_path = Image._get_image_path(data['name'])
 
-            image_path = os.path.join(Image.PUBLIC_FOLDER, data['name'])
             if os.path.exists(image_path):
                 os.remove(image_path)
-                return {"response": "file deleted successfully"}
+                return {"response": "File deleted successfully"}, 200
             else:
                 abort(404, 'Image not found')
+
         except Exception as e:
             return {"error": str(e)}
